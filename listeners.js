0aa5861f631f7b3ea553a2b3170aa5b66caf1bf3
Miles Shang <mail@mshang.ca>
2014-09-26 23:03:25 -0700

    A	index.html A	listeners.js A	spark-md5.js A	starfield.js

---
+++ listeners.js
@@ -0,0 +1,33 @@
+var mousedown = false;
+var lastMouseX;
+var lastMouseY;
+
+canvas.addEventListener("mousedown", function(e) {
+  if (e.button !== 0) return;
+  mousedown = true;
+  lastMouseX = e.pageX;
+  lastMouseY = e.pageY;
+}, false);
+
+document.addEventListener("mousemove", function(e) {
+  if (!mousedown) return;
+  var dX = e.pageX - lastMouseX;
+  var dY = e.pageY - lastMouseY;
+  lastMouseX = e.pageX;
+  lastMouseY = e.pageY;
+  offsetX -= dX / scale;
+  offsetY -= dY / scale;
+}, false);
+
+document.addEventListener("mouseup", function(e) {
+  mousedown = false;
+}, false);
+
+canvas.addEventListener("mousewheel", function(e) {
+  var mult;
+  var rect = canvas.getBoundingClientRect();
+  if (e.wheelDelta > 0) mult = 1.1; else mult = 1 / 1.1;
+  offsetX += (e.clientX - rect.left) * (1 - 1 / mult) / scale;
+  offsetY += (e.clientY - rect.top) * (1 - 1 / mult) / scale;
+  scale *= mult;
+}, false);
---
+++ starfield.js
@@ -0,0 +1,72 @@
+// -- settings, immutable -- //
+var BRIGHTNESS_FACTOR = 10; // Higher = brighter
+var STAR_SIZE_PIXELS = 1.5;
+var STAR_RANGE_INDICES = 10; // Higher = more random looking, comp expensive
+var LEVEL_DEPTH = 5; // Higher = more (faint) stars, comp expensive
+
+// TODO:
+// - touchscreen support
+// - resize / fullscreen
+// - older browsers / polyfill
+// - debug mode with fps counter
+// - cache of md5s
+
+var MAX_INT = -1 >>> 1;
+
+// -- zoom and pan, altered by listeners -- //
+var offsetX = 0; // Top left has this x coordinate in world frame.
+var offsetY = 0;
+// scale of 1 means 1 world unit = 1 canvas pixel
+// scale of 100 means 1 world unit = 100 canvas pixels
+var scale = 1;
+
+var canvas = document.getElementById("starfield");
+canvas.style['background-color'] = 'black';
+var context = canvas.getContext('2d');
+
+function render() {
+  var worldWidth = canvas.width / scale;
+  var worldHeight = canvas.height / scale;
+
+  // The level at which you would expect one star in current viewport.
+  var levelForCurrentScale = - Math.log(worldWidth * worldHeight) / 2
+  var startLevel = Math.floor(levelForCurrentScale);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (level = startLevel; level < startLevel + LEVEL_DEPTH; level++) {
+    var spacing = Math.exp(-level);
+    for (
+      xIndex = Math.floor(offsetX / spacing) - STAR_RANGE_INDICES;
+      xIndex <= Math.ceil((offsetX + worldWidth) / spacing)
+        + STAR_RANGE_INDICES;
+      xIndex++
+    ) {
+      for (
+        yIndex = Math.floor(offsetY / spacing) - STAR_RANGE_INDICES;
+        yIndex <= Math.ceil((offsetY + worldHeight) / spacing)
+          + STAR_RANGE_INDICES;
+        yIndex++
+      ) {
+        var hash = SparkMD5.hash(xIndex + ':' + yIndex + ':' + level, true);
+        var x = xIndex * spacing
+          + (hash[0] / MAX_INT) * spacing * STAR_RANGE_INDICES;
+        var y = yIndex * spacing
+          + (hash[1] / MAX_INT) * spacing * STAR_RANGE_INDICES;
+	var z = Math.atan(
+          Math.exp(-level + (hash[2] / MAX_INT)) * BRIGHTNESS_FACTOR
+            / Math.exp(-levelForCurrentScale)
+        ) * 2 / Math.PI;
+        context.fillStyle = 'rgba(255, 255, 255, ' + z + ')';
+        context.fillRect(
+          (x - offsetX) * scale,
+          (y - offsetY) * scale,
+          STAR_SIZE_PIXELS,
+          STAR_SIZE_PIXELS
+        );
+      }
+    }
+  }
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
