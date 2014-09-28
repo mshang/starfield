e3f43ae0e9f99c07bde48cb18453d55a4711aee1
Miles Shang <mail@mshang.ca>
2014-09-28 12:36:25 -0700

    don't use global namespace

--- render.js
+++ render.js
@@ -1,129 +1,131 @@
 /* vim: set softtabstop=2 shiftwidth=2 expandtab: */
 
-var STAR_SIZE_PIXELS = 1.5;
-var DEBUG = false;
-var timestamps = [];
+(function () {
+  var STAR_SIZE_PIXELS = 1.5;
+  var DEBUG = false;
+  var timestamps = [];
 
-var worldOffsetX = 0; // Top left has this x coordinate in world frame.
-var worldOffsetY = 0;
-// scale of 1 means 1 world unit = 1 canvas pixel
-// scale of 100 means 1 world unit = 100 canvas pixels
-var scale = 1;
+  var worldOffsetX = 0; // Top left has this x coordinate in world frame.
+  var worldOffsetY = 0;
+  // scale of 1 means 1 world unit = 1 canvas pixel
+  // scale of 100 means 1 world unit = 100 canvas pixels
+  var scale = 1;
 
-var mousedown = false;
-var lastMouseX;
-var lastMouseY;
+  var mousedown = false;
+  var lastMouseX;
+  var lastMouseY;
 
-var canvas = document.getElementById("starfield");
-canvas.style['background-color'] = 'black';
+  var canvas = document.getElementById("starfield");
+  canvas.style['background-color'] = 'black';
 
-function render() {
-  var star, stars, i, j, context, brightnessToStars;
+  function render() {
+    var star, stars, i, j, context, brightnessToStars;
+
+    requestAnimationFrame(render);
+
+    stars = getStars(
+      worldOffsetX,
+      worldOffsetY, 
+      canvas.width / scale,
+      canvas.height / scale
+    );
+
+    // setting fillStyle is expensive, so quantize and batch it.
+    brightnessToStars = [];
+    for (i = 0; i < 100; i++) brightnessToStars.push([]);
+    for (i = 0; i < stars.length; i++) {
+      star = stars[i];
+      brightnessToStars[Math.floor(star.brightness * 100)].push(star);
+    }
+
+    context = canvas.getContext('2d')
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (i = 0; i < 100; i++) {
+      context.fillStyle = 'rgba(255, 255, 255, ' + i / 100 + ')';
+      stars = brightnessToStars[i];
+      for (j = 0; j < stars.length; j++) {
+        star = stars[j];
+        context.fillRect(
+          (star.worldX - worldOffsetX) * scale,
+          (star.worldY - worldOffsetY) * scale,
+          STAR_SIZE_PIXELS,
+          STAR_SIZE_PIXELS
+        );
+      }
+    }
+
+    timestamps.push(new Date().getTime());
+    if (timestamps.length > 10) {
+      if (DEBUG) console.log(
+        (9 * 1000 / (timestamps[9] - timestamps[0])).toFixed(2) + ' fps'
+      );
+      timestamps = [];
+    }
+  }
 
   requestAnimationFrame(render);
 
-  stars = getStars(
-    worldOffsetX,
-    worldOffsetY, 
-    canvas.width / scale,
-    canvas.height / scale
-  );
+  canvas.addEventListener("mousedown", function(e) {
+    if (e.button !== 0) return;
+    mousedown = true;
+    lastMouseX = e.pageX;
+    lastMouseY = e.pageY;
+  }, false);
 
-  // setting fillStyle is expensive, so quantize and batch it.
-  brightnessToStars = [];
-  for (i = 0; i < 100; i++) brightnessToStars.push([]);
-  for (i = 0; i < stars.length; i++) {
-    star = stars[i];
-    brightnessToStars[Math.floor(star.brightness * 100)].push(star);
-  }
+  document.addEventListener("mousemove", function(e) {
+    if (!mousedown) return;
+    var dX = e.pageX - lastMouseX;
+    var dY = e.pageY - lastMouseY;
+    lastMouseX = e.pageX;
+    lastMouseY = e.pageY;
+    worldOffsetX -= dX / scale;
+    worldOffsetY -= dY / scale;
+  }, false);
 
-  context = canvas.getContext('2d')
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  document.addEventListener("mouseup", function(e) {
+    mousedown = false;
+  }, false);
 
-  for (i = 0; i < 100; i++) {
-    context.fillStyle = 'rgba(255, 255, 255, ' + i / 100 + ')';
-    stars = brightnessToStars[i];
-    for (j = 0; j < stars.length; j++) {
-      star = stars[j];
-      context.fillRect(
-        (star.worldX - worldOffsetX) * scale,
-        (star.worldY - worldOffsetY) * scale,
-        STAR_SIZE_PIXELS,
-        STAR_SIZE_PIXELS
-      );
-    }
-  }
+  canvas.addEventListener("mousewheel", function(e) {
+    var mult;
+    var rect = canvas.getBoundingClientRect();
+    if (e.wheelDelta > 0) mult = 1.1; else mult = 1 / 1.1;
+    worldOffsetX += (e.clientX - rect.left) * (1 - 1 / mult) / scale;
+    worldOffsetY += (e.clientY - rect.top) * (1 - 1 / mult) / scale;
+    scale *= mult;
+  }, false);
 
-  timestamps.push(new Date().getTime());
-  if (timestamps.length > 10) {
-    if (DEBUG) console.log(
-      (9 * 1000 / (timestamps[9] - timestamps[0])).toFixed(2) + ' fps'
-    );
-    timestamps = [];
-  }
-}
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
 
-requestAnimationFrame(render);
-
-canvas.addEventListener("mousedown", function(e) {
-  if (e.button !== 0) return;
-  mousedown = true;
-  lastMouseX = e.pageX;
-  lastMouseY = e.pageY;
-}, false);
-
-document.addEventListener("mousemove", function(e) {
-  if (!mousedown) return;
-  var dX = e.pageX - lastMouseX;
-  var dY = e.pageY - lastMouseY;
-  lastMouseX = e.pageX;
-  lastMouseY = e.pageY;
-  worldOffsetX -= dX / scale;
-  worldOffsetY -= dY / scale;
-}, false);
-
-document.addEventListener("mouseup", function(e) {
-  mousedown = false;
-}, false);
-
-canvas.addEventListener("mousewheel", function(e) {
-  var mult;
-  var rect = canvas.getBoundingClientRect();
-  if (e.wheelDelta > 0) mult = 1.1; else mult = 1 / 1.1;
-  worldOffsetX += (e.clientX - rect.left) * (1 - 1 / mult) / scale;
-  worldOffsetY += (e.clientY - rect.top) * (1 - 1 / mult) / scale;
-  scale *= mult;
-}, false);
-
-var canvasWidth = canvas.width;
-var canvasHeight = canvas.height;
-
-if (window.navigator.standalone) {
-  document.body.style['height'] = "100%";
-  document.body.style['width'] = "100%";
-  document.body.style['margin'] = "0px";
-  document.documentElement.style['height'] = "100%";
-  document.documentElement.style['width'] = "100%";
-  document.documentElement.style['margin'] = "0px";
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-document.addEventListener("resize", function() {
   if (window.navigator.standalone) {
+    document.body.style['height'] = "100%";
+    document.body.style['width'] = "100%";
+    document.body.style['margin'] = "0px";
+    document.documentElement.style['height'] = "100%";
+    document.documentElement.style['width'] = "100%";
+    document.documentElement.style['margin'] = "0px";
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }
-});
 
-if (typeof screenfull !== "undefined") {
-  document.addEventListener(screenfull.raw.fullscreenchange, function() {
-    if (screenfull.isFullscreen) {
+  document.addEventListener("resize", function() {
+    if (window.navigator.standalone) {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
-    } else {
-      canvas.width = canvasWidth;
-      canvas.height = canvasHeight;
     }
   });
-}
+
+  if (typeof screenfull !== "undefined") {
+    document.addEventListener(screenfull.raw.fullscreenchange, function() {
+      if (screenfull.isFullscreen) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      } else {
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+      }
+    });
+  }
+})();
--- starfield.js
+++ starfield.js
@@ -1,92 +1,98 @@
 /* vim: set softtabstop=2 shiftwidth=2 expandtab: */
 
-// -- settings, immutable -- //
-var BRIGHTNESS_FACTOR = 15; // Higher = brighter
-var STAR_RANGE_INDICES = 10; // Higher = more random looking, comp expensive
-var LEVEL_DEPTH = 5; // Higher = more (faint) stars, comp expensive
+var getStars;
 
-// TODO:
-// - touchscreen support
-// - resize / fullscreen
-// - older browsers / polyfill
+(function () {
+  // -- settings, immutable -- //
+  var BRIGHTNESS_FACTOR = 15; // Higher = brighter
+  var STAR_RANGE_INDICES = 10; // Higher = more random looking, comp expensive
+  var LEVEL_DEPTH = 5; // Higher = more (faint) stars, comp expensive
 
-var MAX_INT = -1 >>> 1;
+  // TODO:
+  // - touchscreen support
+  // - resize / fullscreen
+  // - older browsers / polyfill
 
-function Star(worldX, worldY, brightness) {
-  this.worldX = worldX;
-  this.worldY = worldY;
-  this.brightness = brightness;
-}
+  var MAX_INT = -1 >>> 1;
 
-function hashFnv32a(str) {
-  var i, l,
-    hval = 0x811c9dc5;
-
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) +
-      (hval << 24);
+  function Star(worldX, worldY, brightness) {
+    this.worldX = worldX;
+    this.worldY = worldY;
+    this.brightness = brightness;
   }
-  return hval >> 0;
-}
 
-var cache = new RRCache(100000);
-function cachedHash(to_hash) {
-  var cached = cache.get(to_hash);
-  if (cached) return cached; else {
-    var digest = [
-      hashFnv32a(to_hash + 'a'),
-      hashFnv32a(to_hash + 'b'),
-      hashFnv32a(to_hash + 'c')
-    ];
-    cache.set(to_hash, digest);
-    return digest;
+  function hashFnv32a(str) {
+    var i, l,
+      hval = 0x811c9dc5;
+
+    for (i = 0, l = str.length; i < l; i++) {
+      hval ^= str.charCodeAt(i);
+      hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) +
+        (hval << 24);
+    }
+    return hval >> 0;
   }
-}
 
-function getStars(worldOffsetX, worldOffsetY, worldWidth, worldHeight) {
-
-  // The level at which you would expect one star in current viewport.
-  var levelForCurrentScale = - Math.log(worldWidth * worldHeight) / 2
-  var startLevel = Math.floor(levelForCurrentScale);
-  var stars = [];
-
-  for (level = startLevel; level < startLevel + LEVEL_DEPTH; level++) {
-    var spacing = Math.exp(-level);
-    for (
-      xIndex = Math.floor(worldOffsetX / spacing) - STAR_RANGE_INDICES;
-      xIndex <= Math.ceil((worldOffsetX + worldWidth) / spacing)
-        + STAR_RANGE_INDICES;
-      xIndex++
-    ) {
-      for (
-        yIndex = Math.floor(worldOffsetY / spacing) - STAR_RANGE_INDICES;
-        yIndex <= Math.ceil((worldOffsetY + worldHeight) / spacing)
-          + STAR_RANGE_INDICES;
-        yIndex++
-      ) {
-        var hash = cachedHash(xIndex + ':' + yIndex + ':' + level);
-        stars.push(new Star(
-          xIndex * spacing
-            + (hash[0] / MAX_INT) * spacing * STAR_RANGE_INDICES,
-          yIndex * spacing
-            + (hash[1] / MAX_INT) * spacing * STAR_RANGE_INDICES,
-          Math.max(0,
-	    Math.atan(
-              (
-                Math.exp(
-                  levelForCurrentScale - level - Math.abs(hash[2] / MAX_INT)
-                ) - Math.exp(
-                  // This is to prevent the lowest level of stars from
-                  // suddenly popping in and out with high brightness.
-                  levelForCurrentScale - (startLevel + LEVEL_DEPTH)
-                )
-              ) * BRIGHTNESS_FACTOR
-            ) * 2 / Math.PI
-          )
-        ));
-      }
+  var cache = new RRCache(100000);
+  function cachedHash(to_hash) {
+    var cached = cache.get(to_hash);
+    if (cached) return cached; else {
+      var digest = [
+        hashFnv32a(to_hash + 'a'),
+        hashFnv32a(to_hash + 'b'),
+        hashFnv32a(to_hash + 'c')
+      ];
+      cache.set(to_hash, digest);
+      return digest;
     }
   }
-  return stars;
-}
+
+  function _getStars(worldOffsetX, worldOffsetY, worldWidth, worldHeight) {
+
+    // The level at which you would expect one star in current viewport.
+    var levelForCurrentScale = - Math.log(worldWidth * worldHeight) / 2
+    var startLevel = Math.floor(levelForCurrentScale);
+    var stars = [];
+
+    for (level = startLevel; level < startLevel + LEVEL_DEPTH; level++) {
+      var spacing = Math.exp(-level);
+      for (
+        xIndex = Math.floor(worldOffsetX / spacing) - STAR_RANGE_INDICES;
+        xIndex <= Math.ceil((worldOffsetX + worldWidth) / spacing)
+          + STAR_RANGE_INDICES;
+        xIndex++
+      ) {
+        for (
+          yIndex = Math.floor(worldOffsetY / spacing) - STAR_RANGE_INDICES;
+          yIndex <= Math.ceil((worldOffsetY + worldHeight) / spacing)
+            + STAR_RANGE_INDICES;
+          yIndex++
+        ) {
+          var hash = cachedHash(xIndex + ':' + yIndex + ':' + level);
+          stars.push(new Star(
+            xIndex * spacing
+              + (hash[0] / MAX_INT) * spacing * STAR_RANGE_INDICES,
+            yIndex * spacing
+              + (hash[1] / MAX_INT) * spacing * STAR_RANGE_INDICES,
+            Math.max(0,
+              Math.atan(
+                (
+                  Math.exp(
+                    levelForCurrentScale - level - Math.abs(hash[2] / MAX_INT)
+                  ) - Math.exp(
+                    // This is to prevent the lowest level of stars from
+                    // suddenly popping in and out with high brightness.
+                    levelForCurrentScale - (startLevel + LEVEL_DEPTH)
+                  )
+                ) * BRIGHTNESS_FACTOR
+              ) * 2 / Math.PI
+            )
+          ));
+        }
+      }
+    }
+    return stars;
+  }
+
+  getStars = _getStars;
+})();
